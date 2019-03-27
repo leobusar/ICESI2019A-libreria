3d307d48c0b260ee78a6c81a9403027855836e5d
Leonardo <leobusta@gmail.com>
2019-03-27 09:18:46 -0500

    Updates

---
+++ precache-manifest.1bd77b6c61bc6138fc475d1a3bbe51e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "62bb5a240fdb7958549f",
+    "url": "/ICESI2019A-libreria/static/js/runtime~main.da2ca3a4.js"
+  },
+  {
+    "revision": "80ebf669a519d4bb0404",
+    "url": "/ICESI2019A-libreria/static/js/main.90b66a5b.chunk.js"
+  },
+  {
+    "revision": "e0931aad03b5dc408851",
+    "url": "/ICESI2019A-libreria/static/js/2.fd249ce7.chunk.js"
+  },
+  {
+    "revision": "80ebf669a519d4bb0404",
+    "url": "/ICESI2019A-libreria/static/css/main.459dba25.chunk.css"
+  },
+  {
+    "revision": "3d6344e3184f81c279aee1d8734eb049",
+    "url": "/ICESI2019A-libreria/index.html"
+  }
+];
